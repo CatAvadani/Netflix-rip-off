a38f3bddabda2778951b2074e393064f1f994328
CatAvadani <catalinaavadani@gmail.com>
2024-04-04 14:56:20 +0200

    Create a loading condition  to show before displaying the list of movies.

--- app/myList/page.tsx
+++ app/myList/page.tsx
@@ -2,38 +2,37 @@
 import { movies } from "@/data/movies";
 import { InfoIcon } from "@chakra-ui/icons";
 import { Box, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import { useList } from "../context/MyListContext";
 
 export default function MyListPage() {
-  const { myList, toggleMyList } = useList();
-  const [isLoaded, setIsLoaded] = useState(false);
+  const { myList, toggleMyList, isLoading } = useList();
 
-  useEffect(() => {
-    setIsLoaded(true);
-  }, [myList]);
-  if (!isLoaded || myList.length === 0) {
+  if (isLoading) {
     return (
-      <Box color={"white"} mt={"7rem"}>
-        <Flex direction="column" align="center">
-          <Box textAlign="center" py={10} px={6}>
-            <InfoIcon boxSize={"50px"} color={"red.500"} />
-            <Heading as="h2" size="xl" mt={6} mb={2}>
-              Your list is empty!
-            </Heading>
-          </Box>
-        </Flex>
-      </Box>
+      <Flex justify="center" align="center" height="100vh">
+        <Text color={"red"} fontSize={"1.5rem"}>
+          Loading...
+        </Text>
+      </Flex>
     );
   }
 
   return (
     <Box color={"white"} mt={"7rem"}>
       <Flex direction="column" align="center">
-        <Text fontSize={["2xl", "3xl", "4xl"]} fontWeight="bold" mb={4}>
-          My List
-        </Text>
+        {myList.length > 0 ? (
+          <Text fontSize={["2xl", "3xl", "4xl"]} fontWeight="bold" mb={4}>
+            My List
+          </Text>
+        ) : (
+          <Box textAlign="center" py={10} px={6}>
+            <InfoIcon boxSize={"50px"} color={"red.500"} />
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+              Your list is empty!
+            </Heading>
+          </Box>
+        )}
       </Flex>
       <SimpleGrid
         width="90%"
