db9dc63a4c5cf677cd05e0f0f9fa6780a56177ce
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-04-04 12:28:19 +0200

    add more style and info display when list is empty

--- app/myList/page.tsx
+++ app/myList/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { movies } from "@/data/movies";
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { InfoIcon } from "@chakra-ui/icons";
+import { Box, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import MovieCard from "../components/MovieCard";
 import { useList } from "../context/MyListContext";
 
@@ -8,25 +9,41 @@ export default function MyListPage() {
   const { myList, toggleMyList } = useList();
 
   return (
-    <Box color={"white"} mt={"10rem"}>
-      <SimpleGrid
-        width="80%"
-        m="2rem auto"
-        alignContent={"center"}
-        mt="5rem"
-        columns={{ base: 1, md: 2, lg: 3 }}
-        gap={10}
-      >
-        {myList.map((movieId) => (
-          <MovieCard
-            key={movieId}
-            movie={movies.find((movie) => movieId === movie.id)}
-            isInMyList={true}
-            isExpanded={true}
-            onToggle={() => toggleMyList(movieId)}
-          />
-        ))}
-      </SimpleGrid>
+    <Box color={"white"} mt={"7rem"}>
+      <Flex direction="column" align="center">
+        {myList.length > 0 ? (
+          <Text fontSize={["2xl", "3xl", "4xl"]} fontWeight="bold" mb={4}>
+            My List
+          </Text>
+        ) : (
+          <Box textAlign="center" py={10} px={6}>
+            <InfoIcon boxSize={"50px"} color={"red.500"} />
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+              Your list is empty!
+            </Heading>
+          </Box>
+        )}
+      </Flex>
+      {myList.length > 0 && (
+        <SimpleGrid
+          width="90%"
+          m="2rem auto"
+          alignContent={"center"}
+          mt="2rem"
+          columns={{ base: 1, md: 2, lg: 4 }}
+          columnGap={8}
+        >
+          {myList.map((movieId) => (
+            <MovieCard
+              key={movieId}
+              movie={movies.find((movie) => movieId === movie.id)}
+              isInMyList={true}
+              isExpanded={true}
+              onToggle={() => toggleMyList(movieId)}
+            />
+          ))}
+        </SimpleGrid>
+      )}
     </Box>
   );
 }
